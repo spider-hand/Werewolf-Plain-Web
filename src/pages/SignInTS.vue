bdec646966196666e689d7da8d3088a9e57ae9c8
Spider Hand <creative.spider.hand@gmail.com>
2020-09-04 23:38:30 +0900

    working on sign in and sign up screens

--- src/pages/SignInTS.vue
+++ src/pages/SignInTS.vue
@@ -1,6 +1,40 @@
 <template>
   <div id="page">
-  	
+  	<v-container 
+  		class="sign-in-form-container"
+  		fill-height
+  		fluid>
+  		<v-row>
+	  		<div class="sign-in-form-wrapper">
+	  			<form class="sign-in-form-group">
+	  				<div class="input-wrapper">
+	  					<label class="input-label">EMAIL or USERNAME</label>
+	  					<input class="sign-in-input" type="text" name="username">
+	  				</div>
+	  				<div class="input-wrapper">
+	  					<label class="input-label">PASSWORD</label>
+	  					<input class="sign-in-input" type="password" name="password">
+	  				</div>
+	  				<div class="btn-wrapper">
+	  					<button class="sign-in-btn">SIGN IN</button>
+	  				</div>
+	  			</form>
+	  		</div>
+  			<v-col cols="12">
+  				<div 
+  					class="link-wrapper"
+  					align="center"
+  					justify="center">
+            <span>Don't have an account? </span>
+  					<router-link
+  						class="sign-up-link"
+  						to="/sign-up">
+              Sign up
+            </router-link>
+  				</div>
+  			</v-col>
+  		</v-row>
+  	</v-container>
   </div>
 </template>
 
@@ -12,6 +46,72 @@
   #page {
     position: relative;
     height: 100%;
-    background-color: $black1;
-  }  
+    background-color: $black1 !important;
+  }
+
+  .sign-in-form-wrapper {
+  	width: 500px;
+  	border-radius: 3px;
+  	background-color: $black3 !important;
+  	margin: auto;
+  }
+
+  .form-title {
+  	font-size: 20px;
+  	font-weight: 700;
+  	color: $white !important;
+  }
+
+  .input-wrapper, .btn-wrapper {
+  	width: 100%;
+  	position: relative;
+  	padding: 0 25px 0 25px;
+  	margin: 30px 0 30px 0;
+  }
+
+  .input-label {
+  	color: $gray3;
+  	font-size: 12px;
+  	font-weight: 500;
+  }
+
+  .sign-in-input {
+  	font-size: 16px;
+  	width: 100%;
+  	height: 45px;
+  	color: $white !important;
+  	background-color: $black4 !important;
+  	border-radius: 3px;
+  	border: 1.5px solid $black5;
+  	padding: 0 10px 0 10px;
+  }
+
+  .sign-in-input:focus {
+  	outline: none;
+  }
+
+  .sign-in-btn {
+  	font-size: 16px;
+  	width: 100%;
+  	height: 50px;
+  	background-color: $red1 !important;
+  	color: $white !important;
+  	border-radius: 3px;
+  }
+
+  .sign-up-link {
+  	font-size: 14px;
+    font-weight: 500;
+  	color: $white !important;
+  	text-decoration: none !important;
+  }
+
+  .link-wrapper {
+  	margin: 0 0 5px 30px;
+  }
+
+  .link-wrapper span {
+    font-size: 14px;
+    color: $gray1 !important;
+  }
 </style>
