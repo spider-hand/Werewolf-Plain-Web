54eb818b19b129a1dcb78c94393be8be8bd843e2
Spider Hand <creative.spider.hand@gmail.com>
2020-09-05 00:34:48 +0900

    add sign in and sign up buttons

--- src/components/bar/HeaderTS.vue
+++ src/components/bar/HeaderTS.vue
@@ -6,6 +6,20 @@
         width="120">
       </v-img>
     </div>
+    <div class="flex-grow-1"></div>
+    <v-btn
+    	class="sign-in-btn" 
+    	text
+    	@click="$router.push({ name: 'sign-in' })">
+    	<span>Sign In</span>
+    </v-btn>
+    <v-btn
+    	class="sign-up-btn"
+    	rounded
+    	depressed
+    	@click="$router.push({ name: 'sign-up' })">
+    	<span>Sign Up</span>
+    </v-btn>
   </v-app-bar>
 </template>
 
@@ -16,5 +30,14 @@
 <style lang="scss" scoped>
  .header {
   background-color: $black1 !important;
+ }
+
+ .sign-in-btn {
+ 	color: $red1 !important;
+ 	text-transform: none;
+ }
+
+ .sign-up-btn {
+ 	text-transform: none;
  }
 </style>
