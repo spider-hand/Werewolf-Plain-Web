1e2bfa5ddaea5494ee39df4b99a26e5fa2c3262a
Spider Hand <creative.spider.hand@gmail.com>
2020-09-12 23:42:15 +0900

    sign out user

--- src/components/bar/HeaderTS.vue
+++ src/components/bar/HeaderTS.vue
@@ -7,24 +7,65 @@
       </v-img>
     </div>
     <div class="flex-grow-1"></div>
-    <v-btn
-    	class="sign-in-btn" 
-    	text
-    	@click="$router.push({ name: 'sign-in' })">
-    	<span>Sign In</span>
-    </v-btn>
-    <v-btn
-    	class="sign-up-btn"
-    	rounded
-    	depressed
-    	@click="$router.push({ name: 'sign-up' })">
-    	<span>Sign Up</span>
-    </v-btn>
+    <div v-if="state.isSignedIn">
+      <DialogSettings />
+      <DialogUserSignOut />
+    </div>
+    <div v-else>
+      <v-btn
+        class="sign-in-btn" 
+        text
+        @click="$router.push({ name: 'sign-in' })">
+        <span>Sign In</span>
+      </v-btn>
+      <v-btn
+        class="sign-up-btn"
+        rounded
+        depressed
+        @click="$router.push({ name: 'sign-up' })">
+        <span>Sign Up</span>
+      </v-btn>
+    </div>
   </v-app-bar>
 </template>
 
 <script lang="ts">
-  
+  import { defineComponent, reactive, onMounted, } from '@vue/composition-api'
+
+  import firebase from 'firebase/app'
+  import 'firebase/auth'
+
+  import DialogSettings from '@/components/dialog/DialogSettingsTS.vue'
+  import DialogUserSignOut from '@/components/dialog/DialogUserSignOutTS.vue'
+
+  export default defineComponent({
+    components: {
+      DialogSettings,
+      DialogUserSignOut,
+    },
+
+    setup(props, context) {
+      const state = reactive<{
+        isSignedIn: boolean,
+      }>({
+        isSignedIn: false,
+      })
+
+      onMounted(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+            state.isSignedIn = true
+          } else {
+            state.isSignedIn = false
+          }
+        })
+      })
+
+      return {
+        state,
+      }
+    }
+  })
 </script>
 
 <style lang="scss" scoped>
@@ -34,10 +75,9 @@
 
  .sign-in-btn {
  	color: $red1 !important;
- 	text-transform: none;
  }
 
- .sign-up-btn {
+ .sign-in-btn, .sign-up-btn {
  	text-transform: none;
  }
 </style>
---
+++ src/components/dialog/DialogUserSignOutTS.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-dialog
+    persistent
+    max-width="400"
+    v-model="state.dialog">
+    <template v-slot:activator="{ on }">
+      <v-btn
+        class="sign-out-btn"
+        text
+        v-on="on">
+        <span>Sign Out</span>
+      </v-btn>
+    </template>
+    <v-card class="dialog-wrapper">
+      <v-card-title class="dialog-title">
+        <span>Sign Out</span>
+      </v-card-title>
+      <v-card-text class="dialog-text">
+        <v-container>
+          <span>Are you sure you want to sign out?</span>
+        </v-container>
+      </v-card-text>
+      <v-card-actions class="dialog-actions">
+        <div class="flex-grow-1"></div>
+        <v-btn 
+          class="confirm-btn"
+          depressed
+          @click="signOut"
+          >
+          <span>SIGN OUT</span>
+        </v-btn>
+        <v-btn
+          class="cancel-btn"
+          text
+          @click="cancel">
+          <span>CLOSE</span>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+  import { defineComponent, reactive, } from '@vue/composition-api'
+
+  import firebase from 'firebase/app'
+  import 'firebase/auth'
+
+  export default defineComponent({
+
+    setup(props, context) {
+
+      const state = reactive<{
+        dialog: boolean,
+      }>({
+        dialog: false,
+      })
+
+      function signOut(): void {
+        firebase.auth().signOut().then(() => {
+
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
+
+      function cancel(): void {
+        state.dialog = false
+      }
+
+      return {
+        state,
+        signOut,
+        cancel,
+      }
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .sign-out-btn {
+  color: $red1 !important;
+  text-transform: none;
+  }
+
+  .dialog-wrapper {
+    background-color: $black3 !important;
+  }
+
+  .dialog-text span {
+    color: $gray4;
+  }
+
+  .dialog-actions {
+    background-color: $black2;
+  }
+
+  .dialog-title, .confirm-btn, .cancel-btn span {
+    color: $white;
+  }
+
+  .dialog-title span {
+    font-size: 16px;
+  }
+
+  .confirm-btn {
+    background-color: $red1 !important;
+  }
+
+  .confirm-btn, .cancel-btn span {
+    font-size: 12px;
+  }
+</style>
