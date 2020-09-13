3968f6be6c88a28f15395b726a73ebe85253c6ed
Spider Hand <creative.spider.hand@gmail.com>
2020-09-13 16:14:12 +0900

    SettingsDialog design

--- src/components/dialog/DialogSettingsTS.vue
+++ src/components/dialog/DialogSettingsTS.vue
@@ -16,14 +16,82 @@
         <span>Settings</span>
       </v-card-title>
       <v-card-text class="dialog-text">
+        <v-container v-if="!state.isEditing">
+          <v-row>
+            <v-col 
+              cols="12"
+              class="pt-0 pb-0">
+              <span class="in-game-name-text">In game name</span>
+            </v-col>
+            <v-col 
+              cols="12"
+              class="pt-0">
+              <span class="in-game-name-value">Anonymous</span>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col 
+              cols="12"
+              class="pt-0 pb-0">
+              <span class="avatar-text">Avatar</span>
+            </v-col>
+            <v-col cols="12">
+              <v-img
+                class="avatar"
+                src="@/assets/logo.png">
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-container v-else>
+          <form>
+            <div class="input-wrapper">
+              <label class="input-label">In game name</label>
+              <input 
+                class="settings-input" 
+                type="text" 
+                name="in-game-name">
+            </div>
+            <v-btn
+              class="upload-avatar-btn"
+              depressed>
+              <v-icon 
+                class="upload-icon"
+                left>mdi-upload</v-icon>
+              <span>UPLOAD AVATAR</span>
+            </v-btn>
+          </form>
+        </v-container>
       </v-card-text>
       <v-card-actions class="dialog-actions">
         <div class="flex-grow-1"></div>
         <v-btn
+          class="edit-btn"
+          depressed
+          v-if="!state.isEditing"
+          @click="edit">
+          <span>EDIT</span>
+        </v-btn>
+        <v-btn
+          class="save-btn"
+          depressed
+          v-else
+          @click="update">
+          <span>SAVE</span>
+        </v-btn>
+        <v-btn
+          class="close-btn"
+          text
+          v-if="!state.isEditing"
+          @click="close">
+          <span>CLOSE</span>
+        </v-btn>              
+        <v-btn
           class="cancel-btn"
           text
+          v-else
           @click="cancel">
-          <span>CLOSE</span>
+          <span>CANCel</span>
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -39,17 +107,34 @@
 
       const state = reactive<{
         dialog: boolean,
+        isEditing: boolean,
       }>({
         dialog: false,
+        isEditing: false,
       })
 
+      function edit(): void {
+        state.isEditing = true
+      }
+
+      function update(): void {
+
+      }
+
       function cancel(): void {
+        state.isEditing = false
+      }
+
+      function close(): void {
         state.dialog = false
       }
 
       return {
         state,
+        edit,
+        update,
         cancel,
+        close,
       }
     }
   })
@@ -65,23 +150,69 @@
     background-color: $black3 !important;
   }
 
-  .dialog-text span {
-    color: $gray4;
-  }
-
   .dialog-actions {
     background-color: $black2;
   }
 
-  .dialog-title, .cancel-btn span {
-    color: $white;
-  }
-
   .dialog-title span {
+    color: $white;
     font-size: 16px;
   }
 
-  .cancel-btn span {
+  .in-game-name-text, .avatar-text {
+    color: $gray4;
     font-size: 12px;
+  }
+
+  .in-game-name-value {
+    color: $white;
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  .edit-btn, .save-btn, .upload-avatar-btn {
+    background-color: $red1 !important;
+  }
+
+  .edit-btn, .save-btn, .close-btn, .cancel-btn, .upload-avatar-btn span {
+    color: $white !important;
+    font-size: 12px;
+  }
+
+  .input-wrapper {
+    width: 100%;
+    position: relative;
+    margin: 20px 0 20px 0;
+  }
+
+  .input-label {
+    color: $gray3;
+    font-size: 12px;
+    font-weight: 500;
+  }
+
+  .settings-input {
+    font-size: 16px;
+    width: 100%;
+    height: 45px;
+    color: $white;
+    background-color: $black4;
+    border-radius: 3px;
+    border: 1.5px solid $black5;
+    padding: 0 10px 0 10px;
+  }
+
+  .settings-input:focus {
+    outline: none;
+  }
+
+  .upload-icon {
+    color: $white !important;
   }
 </style>
